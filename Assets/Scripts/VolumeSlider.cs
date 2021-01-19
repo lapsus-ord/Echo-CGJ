584e3b28ec1f91372b28b1f2307b9f139a4d5e4d
Rotashin <mateo.fernandez73@gmail.com>
2021-01-19 12:57:58 +0100

    Map (Spawn) + Menu Pause

--- Assets/Scripts/VolumeSlider.cs
+++ Assets/Scripts/VolumeSlider.cs
@@ -9,7 +9,7 @@ public class VolumeSlider : MonoBehaviour
     public UnityEngine.Audio.AudioMixer mixer;
     public string parameterName;
 
-    void Awake () 
+    void Awake() 
     {
         float savedVol = PlayerPrefs.GetFloat(parameterName, slider.maxValue);
         SetVolume(savedVol); // Manually set value & volume before subscribing to ensure it is set even if slider.value happens to start at the same value as is saved
@@ -17,7 +17,7 @@ public class VolumeSlider : MonoBehaviour
         slider.onValueChanged.AddListener((float _) => SetVolume(_)); // UI classes use unity events, requiring delegates (delegate(float _) { SetVolume(_); }) or lambda expressions ((float _) => SetVolume(_))
     }
      
-    void SetVolume (float _value)
+    void SetVolume(float _value)
     {
         mixer.SetFloat(parameterName, ConvertToDecibel(_value/slider.maxValue)); // Dividing by max allows arbitrary positive slider maxValue
         PlayerPrefs.SetFloat(parameterName, _value);
@@ -27,7 +27,7 @@ public class VolumeSlider : MonoBehaviour
     ///     Converts a percentage fraction to decibels,
     ///     with a lower clamp of 0.0001 for a minimum of -80dB, same as Unity's Mixers.
     /// </summary>
-    public float ConvertToDecibel (float _value)
+    public float ConvertToDecibel(float _value)
     {
         return Mathf.Log10(Mathf.Max(_value, 0.0001f))*20f;
     }
