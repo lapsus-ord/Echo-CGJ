31753528fdd24d53ac0877684d67e6202f227da5
Rotashin <mateo.fernandez73@gmail.com>
2021-01-20 00:02:59 +0100

    Script Cursor en Crosshair

---
+++ Assets/Scripts/CursorTexture.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorTexture : MonoBehaviour
+{
+    public Texture2D cursorTexture;
+    public CursorMode cursorMode = CursorMode.ForceSoftware;
+    public Vector2 cursorOffset = Vector2.zero;
+    
+    private void Awake()
+    {
+        Cursor.SetCursor(cursorTexture, cursorOffset, cursorMode);
+    }
+}
