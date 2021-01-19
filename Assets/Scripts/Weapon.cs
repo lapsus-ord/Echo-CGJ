05f7e90309b2475c7c8c8c3df42006b0018dbb5c
Rotashin <mateo.fernandez73@gmail.com>
2021-01-19 21:19:39 +0100

    Crosshair et pistolet

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -1,22 +1,47 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
     public Transform firePoint;
-    
+    public GameObject bulletPrefab;
+    public float bulletSpeed = 20f;
+
+    public GameObject crosshair;
+    private Vector3 target;
+
+    private void Start()
+    {
+        Cursor.visible = false;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButton("Fire1"))
+        target = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.position.z));
+        crosshair.transform.position = new Vector2(target.x, target.y);
+
+        Vector3 difference = target - firePoint.transform.position;
+        float rotationZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        
+        if (Input.GetButtonDown("Fire1"))
         {
-            Debug.Log("Clic droit !");
+            float distance = difference.magnitude;
+            Vector2 direction = difference / distance;
+            direction.Normalize();
+            fireBullet(direction, rotationZ);
         }
     }
 
-    void Shoot()
+    void fireBullet(Vector2 direction, float rotationZ)
     {
-        
+        GameObject b = Instantiate(bulletPrefab) as GameObject;
+        b.transform.position = firePoint.transform.position;
+        b.transform.rotation = quaternion.Euler(0.0f, 0.0f, rotationZ);
+
+        b.GetComponent<Rigidbody2D>().velocity = direction * bulletSpeed;
     }
 }
