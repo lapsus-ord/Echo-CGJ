4ac698cb1fb6f66729a1f809cfd57cd401393053
Rotashin <mateo.fernandez73@gmail.com>
2021-01-19 12:58:59 +0100

    Script HUD

---
+++ Assets/Scripts/PlayerHUD.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHUD : MonoBehaviour
+{
+    public GameObject HUD;
+    public GameObject menuPause;
+    
+    private void Update()
+    {
+        if (Input.GetKeyDown("escape"))
+        {
+            HUD.SetActive(!HUD.activeSelf);
+            menuPause.SetActive(!HUD.activeSelf);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+    private Vector2 movement;
+
+    public GameObject menuPause;
+    private Boolean isInPause = false;
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        // Si on est dans le menu, on ne peut pas bouger
+        isInPause = menuPause.activeSelf;
+    }
+
+    private void FixedUpdate()
+    {
+        if (!isInPause)
+        {
+            rb.MovePosition(rb.position + movement * (moveSpeed * Time.fixedDeltaTime));
+        }
+    }
+}
