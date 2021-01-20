04f4b0c2f6dab16df3125500e8bef0e4ed6d82cc
yetnaive <77579787+yetnaive@users.noreply.github.com>
2021-01-20 04:06:59 +0000

    début boss

---
+++ Assets/Scripts/Boss.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour
+{
+    public GameObject sortie;
+    public GameObject sortieTile;
+    public AudioSource odio;
+    public bouleDeFeu boulasse;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        sortie = GameObject.Find("Sortie");
+        sortieTile = GameObject.Find("Grid/fg2");
+        sortie.SetActive(false);
+        sortieTile.SetActive(false);
+        sonRun(odio);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void sonRun(AudioSource odio) {
+        odio.PlayDelayed(0);
+    }
+}
---
+++ Assets/Scripts/bouleDeFeu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bouleDeFeu : MonoBehaviour
+{
+    public bool versDroite;
+        void Update(){
+            if (versDroite){
+                transform.position = transform.position + new Vector3(5f * Time.deltaTime, 0, 0) ;
+            } else {
+                transform.position = transform.position + new Vector3(-5f * Time.deltaTime, 0, 0) ;
+            }
+        }
+}
