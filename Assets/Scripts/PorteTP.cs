92fff4cad097caf2611088bf9090342076ce8e00
Rotashin <mateo.fernandez73@gmail.com>
2021-01-20 02:48:03 +0100

    Déplacement inter-scènes (TP Porte)

--- Assets/Scripts/PorteTP.cs
+++ Assets/Scripts/PorteTP.cs
@@ -8,11 +8,14 @@ public class PorteTP : MonoBehaviour
     // Start is called before the first frame update
     public string SceneToLoad;
     public Vector2 playerPosition;
-    public VectorValue playerStorage;
+    public GameObject player;
 
     private void OnTriggerEnter2D(Collider2D other) {
-        if(other.CompareTag("Player") && !other.isTrigger){
-            playerStorage.initialValue = playerPosition;
+        if(other.CompareTag("Player") && !other.isTrigger)
+        {
+            PlayerPrefs.SetFloat("posX", playerPosition.x);
+            PlayerPrefs.SetFloat("posY", playerPosition.y);
+
             SceneManager.LoadScene(SceneToLoad);
         }
     }
