27162ea6345dfd8670d184d025302bc1ea7f4643
Rotashin <mateo.fernandez73@gmail.com>
2021-01-20 07:38:48 +0100

    Correction script Health

---
+++ Assets/Scripts/PlayerprefsValue.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerprefsValue : MonoBehaviour
+{
+    void Start()
+    {
+        PlayerPrefs.SetInt("valueHealth", 18);
+        PlayerPrefs.SetFloat("posX", 1.5f);
+        PlayerPrefs.SetFloat("posX", -0.2f);
+    }
+}
