d4063f12cfa7764aebd31344d22fad6143569c5b
yetnaive <77579787+yetnaive@users.noreply.github.com>
2021-01-20 05:33:59 +0000

    début boss

--- Assets/Scripts/Boss.cs
+++ Assets/Scripts/Boss.cs
@@ -7,7 +7,10 @@ public class Boss : MonoBehaviour
     public GameObject sortie;
     public GameObject sortieTile;
     public AudioSource odio;
-    public bouleDeFeu boulasse;
+    public Enemy Slime1;
+    public Enemy Slime2;
+    public Enemy Slime3;
+    public GameObject oeil;
     // Start is called before the first frame update
     void Awake()
     {
@@ -21,7 +24,10 @@ public class Boss : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Slime1.health<=0&&Slime2.health<=0&&Slime3.health<=0){
+            oeil.gameObject.SetActive(false);
+            AudioListener.volume = 0;
+        }
     }
     void sonRun(AudioSource odio) {
         odio.PlayDelayed(0);
