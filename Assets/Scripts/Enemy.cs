0a6f067fb3684e20361da97a1bcf43b8f38a2104
yetnaive <77579787+yetnaive@users.noreply.github.com>
2021-01-20 01:58:42 +0000

    Ajoute ennemi etc

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int health;
+    public string enemy_name;
+    public int baseAttack;
+    public float moveSpeed;
+    public AudioSource odio;
+
+void Start() {
+    
+}
+private void OnTriggerEnter2D(Collider2D other) {
+    Debug.Log(" test 2");
+    if(other.CompareTag("balle")){
+        sonRun(odio);
+        Debug.Log(this.health);
+        this.health -= 1;
+        if (this.health <= 0) {
+            Death();
+        }
+    }
+}
+void Death () {
+    this.gameObject.SetActive(false);
+    Debug.Log("MOOOOOORT");
+}
+void sonRun(AudioSource odio) {
+    odio.PlayDelayed(0);
+}
+}
